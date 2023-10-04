9d5f46ea4d56ed69267ea213e9a1688de5a408f2
Sitinurfaiza <112322032+Sitinurfaiza@users.noreply.github.com>
2023-10-04 10:23:53 +0700

    Add files via upload

---
+++ Laporan_3/panggil_include.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>siti_nurfaiza</title>
+</head>
+<body>
+    <?php
+    for ($b = 1; $b < 5; $b++) {
+        include("contoh_include.php");
+    }
+    ?>
+</body>
+</html>
