678ed5f975e6ad0df535afe9c1d0616e644caa0b
Sitinurfaiza <112322032+Sitinurfaiza@users.noreply.github.com>
2023-10-17 22:59:25 +0700

    Add files via upload

---
+++ Laporan_5/mysqli_fetch_array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koneksi Database MySQL</title>
+</head>
+<body>
+    <h1>Koneksi Database dengan mysqli_fetch_array</h1>
+    <?php 
+    $conn = mysqli_connect("localhost","root","", "db_saya") 
+    or die("koneksi gagal");
+    $hasil = mysqli_query($conn, "select * from liga");
+    while ($row = mysqli_fetch_array($hasil)) {
+    echo "liga" . $row["Negara"]; //array asosiatif
+    echo "mempunyai" . $row[2]; //array nummeris
+    echo " wakil di liga champion <br>";
+    }
+    ?>
+</body>
+</html>
